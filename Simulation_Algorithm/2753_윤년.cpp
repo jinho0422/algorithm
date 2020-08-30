973da76192ab5f0435e57ee3683348be07612c83
방진호 <bangjinho0422@gmail.com>
2020-08-30 21:43:24 +0900

    simulation

---
+++ Simulation_Algorithm/2753_윤년.cpp
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <cstdio>
+
+using namespace std;
+
+int year;
+
+int main() {
+	scanf("%d", &year);
+	if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) printf("1\n");
+	else printf("0\n");
+
+	return 0;
+}
